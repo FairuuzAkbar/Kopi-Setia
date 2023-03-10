fabd4a9b96b898c52318d0d6f6a58b88e12cc369
Fairuuz Akbar <fairuzakbr27@gmail.com>
2023-03-10 22:36:06 +0700

    membuat website kopi setia

---
+++ script.js
@@ -0,0 +1,8 @@
+// toggle class active
+const navbarNav = document.querySelector
+('.navbar-nav');
+// Ketika di klik
+document.querySelector('#hamburger-menu').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+};
